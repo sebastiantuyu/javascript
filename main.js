97b5b048d82a84865782cc6f93d89295dd2e4769
sebastiantuyu <alextpineiro@gmail.com>
2021-01-07 17:24:37 -0600

    Initial values to js-files

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// Valores basicos javascript
+    40
+
+    "String test"
+
+    true
+    false
+// Valor faltante, pero dando el espacio en memoria
+    null
+    undefined
+
+// Valor tipo objeto
+
+    [1,2,3]
+
+// Agregando objetos
+    {'nombre': 'testing'};
