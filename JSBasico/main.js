ce3de3f70a4dbe7961ad90c538dbcb5e3e6a838b
sebastiantuyu <alextpineiro@gmail.com>
2021-01-11 12:18:26 -0600

    New files for different js course

---
+++ JSBasico/main.js
@@ -0,0 +1,15 @@
+// Objetos 
+
+
+var persona ={
+    nombre:'Sebastian',
+    apellido:'Tuyu',
+    edad:'20'
+}
+
+function imprimirNombreMayus(persona){
+    console.log(persona.toUpperCase())
+}
+
+
+imprimirNombreMayus(persona.nombre);
