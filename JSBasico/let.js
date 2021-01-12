528363ce1110c869903e79c6273e44c0a74cf6c6
Sebastian Tuyu <alextpineiro@gmail.com>
2021-01-12 15:54:53 -0600

    Let, var test

---
+++ JSBasico/let.js
@@ -0,0 +1,13 @@
+var persona = {
+    fname:'sebastian',
+    lname:'tuyu',
+    age:21
+}
+
+
+for(var i=0;i<10;i++){
+    //console.log(`1: ${i}`)
+    setTimeout(()=>console.log(i),0)
+    //console.log(`2: ${i}`)
+}
+
