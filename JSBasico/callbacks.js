414f1c57a559297b720215674495935bdffdbfc8
sebastiantuyu <alextpineiro@gmail.com>
2021-01-12 13:31:11 -0600

    Error handler

--- JSBasico/callbacks.js
+++ JSBasico/callbacks.js
@@ -6,20 +6,31 @@ const PEOPLE_URL = 'people/:id'
 /* const FULL_URL=`${ROOT_URL}${PEOPLE_URL.replace(':id',1)}` */
 
 const options = { crossDomain:true }
-const onPeopleResponse = function(people){
+/* const onPeopleResponse = function(people){
     console.log(`ID:(${people.result.uid}) ${people.result.properties.name}`)
 }
+ */
 
 
-
-function getPersona(id){
+function getPersona(id,callback){
     
     const URL =`${ROOT_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(URL, options, onPeopleResponse)
     
+    $.get(URL, options, function(people){
+        console.log(`ID:(${people.result.uid}) ${people.result.properties.name}`)
+    })
+    
+    if(callback){
+        callback()
+    }
 }
 
-getPersona(1)
-getPersona(2)
-getPersona(3)
+
+/* SOLICITAR REQUEST EN SERIE */
+getPersona(1,
+     () => getPersona(2,
+         () => getPersona(3,
+            () => getPersona(4,
+               ()=>getPersona(5) 
+            ))))
 
